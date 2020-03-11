fc8d8afdc1721093c624a532d8316f8bf9a12939
Taylor McCants <taylor.mccants@uzh.ch>
2020-03-11 10:20:06 +0100

    Group03 rocks header

--- src/views/Header.js
+++ src/views/Header.js
@@ -32,7 +32,7 @@ const Title = styled.h1`
 const Header = props => {
   return (
     <Container height={props.height}>
-      <Title>SoPra FS20 rocks with React!</Title>
+      <Title>SoPra Group03 rocks!</Title>
       <ReactLogo width={60} height={60} />
     </Container>
   );
