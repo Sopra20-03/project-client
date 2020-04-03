abc78fd69600bfb7ed1172921b40dcdb40f5ce66
taylor <taylor.mccants@uzh.ch>
2020-04-03 10:55:44 +0200

    FS20G03-6 #comment created Game Details page

--- src/components/lobby/GameDetails.js
+++ src/components/lobby/GameDetails.js
@@ -3,68 +3,97 @@ import styled from 'styled-components';
 import {BaseContainer} from '../../helpers/layout';
 import {api, handleError} from '../../helpers/api';
 import {withRouter} from 'react-router-dom';
-import {Button} from '../../views/design/Button';
-import JustOneGame from "../shared/models/JustOneGame";
+import {Button} from '../../views/design/Button/Button';
+import Game from "../shared/models/Game";
 
 const FormContainer = styled.div`
   margin-top: 2em;
   display: flex;
-  webkit-borderRadius: 1em;
-  background: #ffffff;
-  boxShadow: '0 30px 60px 0 rgba(0,0,0,0.3)';
-  flex-direction: column;
   align-items: center;
-  min-height: 300px;
+  flex-direction: column;
   justify-content: center;
+  width: 100%;
+  min-height: 100%;
 `;
 
-const Form = styled.div`
-  WebkitBorderRadius: 10px 10px 10px 10px;
-  borderRadius: 5px 5px 5px 5px;
-  padding: 1.2rem;
-  width: 90%;
-  maxWidth: 450px;
-  position: relative;
-  boxShadow: 0 30px 60px 0 rgba(0,0,0,0.3);
-  textAlign: center;
-`;
-
-const FormTitle = styled.div`
-    margin-bottom: 30px;
+const FormContent = styled.div`
+    --webkit-border-radius: 10px 10px 10px 10px;
+    border-radius: 5px 5px 5px 5px;
+    background: #ffffff;
+    padding: 1.2rem;
+    width: 90%;
+    maxwidth: 450px;
+    position: relative;
+    box-shadow: 0 30px 60px 0 rgba(0, 0, 0, 0.3);
     text-align: center;
-    font-size: 40px;
 `;
 
-const InputField = styled.input`
-  &::placeholder {
-    color: rgba(255, 255, 255);
-  }
-  height: 35px;
-  padding-left: 15px;
-  margin-left: 10px;
+const FormHeader = styled.div`
+  font-size: 50px;
+  font-weight: bold;
+  text-transform: uppercase;
+  letter-spacing: 10px;
+  margin: auto;
+`;
+
+const TextInput = styled.input`
+    background-color: #f3ead8;
   border: none;
-  border-radius: 20px;
-  margin-bottom: 20px;
-  background: rgba(243, 234, 216);
-  color: black;
-`;
-
-const Label = styled.label`
-  color: black;
-  margin-bottom: 10px;
+  color: #0d0d0d;
+  padding: 0.5rem 0.8rem;
+  margin: 0.5rem 1rem;
   text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  width: 85%;
+  border: 2px solid;
+  transition: all 0.5s ease-in-out;
+  border-radius: 5px;
+
+  &::focus {
+    background-color: #fff;
+    border-bottom: 2px solid #5fbae9;
+    border-radius: 5px;
+  }
 `;
 
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 20px;
-`;
+const textOrange = {
+    color: '#ff7b00'
+};
+
+const textRed = {
+    color: '#de0006'
+};
+
+const textPink = {
+    color: '#ff0091'
+};
+
+const textViolet = {
+    color: '#80228f'
+};
+
+const textBlue = {
+    color: '#00a3e9'
+};
+
+const textGreen = {
+    color: '#00a839'
+};
+
+const textYellow = {
+    color: '#ffd700'
+};
+
+const textBlack = {
+    color: '#000000'
+};
 
 /**
  * @Class
  */
-class CreateGame extends React.Component {
+class GameDetails extends React.Component {
     /**
      * The initial state is defined in the constructor. The state is a JS object containing two fields: gameName and creator
      * These fields are then handled in the onChange() methods in the resp. InputFields
@@ -89,7 +118,7 @@ class CreateGame extends React.Component {
             const response = await api.post('/games', requestBody);
 
             // Get the returned game and update a new object.
-            const game = new JustOneGame(response.data);
+            const game = new Game(response.data);
 
             // Game creation successfully worked --> navigate to the route /lobby in the GameRouter
             this.props.history.push(`/lobby`);
@@ -122,27 +151,24 @@ class CreateGame extends React.Component {
         return (
             <BaseContainer>
                 <FormContainer>
-                    <FormTitle>Create Game Form</FormTitle>
-                    <Form>
-                        <Label>Game Name</Label>
-                        <InputField
-                            placeholder="Enter here.."
-                            onChange={e => {
-                                this.handleInputChange('gameName', e.target.value);
-                            }}
-                        />
-                        <ButtonContainer>
-                            <Button
-                                disabled={!this.state.gameName}
-                                width="50%"
-                                onClick={() => {
-                                    this.createGame();
-                                }}
-                            >
-                                Create Game
-                            </Button>
-                        </ButtonContainer>
-                    </Form>
+                    <FormContent>
+                        <FormHeader>
+                            <span style={textOrange}>G</span>
+                            <span style={textRed}>a</span>
+                            <span style={textPink}>m</span>
+                            <span style={textViolet}>e </span>
+                            <span style={textBlue}>D</span>
+                            <span style={textGreen}>e</span>
+                            <span style={textYellow}>t</span>
+                            <span style={textBlack}>a</span>
+                            <span style={textOrange}>i</span>
+                            <span style={textRed}>l</span>
+                            <span style={textPink}>s</span>
+                        </FormHeader>
+
+                        <TextInput onChange={e => {this.handleInputChange('gameName', e.target.value);}} type='text' placeholder='Game Name'></TextInput>
+                        <Button>Create Game</Button>
+                    </FormContent>
                 </FormContainer>
             </BaseContainer>
         );
@@ -153,4 +179,4 @@ class CreateGame extends React.Component {
  * You can get access to the history object's properties via the withRouter.
  * withRouter will pass updated match, location, and history props to the wrapped component whenever it renders.
  */
-export default withRouter(CreateGame);
+export default withRouter(GameDetails);
