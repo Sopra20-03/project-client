fd2822dd8e94f5874d1e88c3dad377d01ae9e6bf
Jonas Wittwer <j94wittwer@gmail.com>
2020-04-06 14:35:46 +0200

    FS20G03-27 implemented registering logic

--- src/components/login/Register.js
+++ src/components/login/Register.js
@@ -4,6 +4,8 @@ import {BaseContainer} from "../../helpers/layout";
 import {FormContainer, FormContent, FormHeader, Link, TextInput} from "./Login";
 import Colors from "../../views/design/Colors";
 import Button from "../../views/design/Button";
+import User from "../shared/models/User";
+import {api, handleError} from "../../helpers/api";
 
 class Register extends React.Component {
 
@@ -18,7 +20,23 @@ class Register extends React.Component {
   }
 
   async register() {
-    // TODO implement register method
+    try {
+      const requestBody = JSON.stringify({
+        name: this.state.name,
+        username: this.state.username,
+        password: this.state.password
+      });
+
+      const response = await api.post('/users', requestBody);
+
+      const user = new User(response.data);
+
+      localStorage.setItem('token', user.token);
+
+      this.props.history.push(`/login`);
+    } catch (error) {
+      alert(handleError(error));
+    }
   }
 
   handleInputChange(key, value) {
@@ -53,7 +71,11 @@ class Register extends React.Component {
               this.handleInputChange('password', e.target.value);
             }} type='password' placeholder='Password'></TextInput>
             <br></br>
-            <Button>Register</Button>
+            <Button
+              disabled={!this.state.name || !this.state.username || !this.state.password}
+              onClick={() => {
+                this.register();
+              }}>Register</Button>
             <br></br>
             <Link href="/login">Already have an account? Login here!</Link>
           </FormContent>
