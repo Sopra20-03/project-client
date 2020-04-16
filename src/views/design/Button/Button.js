fb56b6fcf931b676f16db24bbcae35c311cda5d6
taylor <taylor.mccants@uzh.ch>
2020-04-16 11:40:15 +0200

    FS20G03-107 #comment upgraded Button design

--- src/views/design/Button/Button.js
+++ src/views/design/Button/Button.js
@@ -1,22 +1,16 @@
-import styled from "styled-components";
+import React from 'react';
+import {withStyles} from '@material-ui/core/styles'
+import {Button} from '@material-ui/core';
 
-const Button = styled.button`
-  display: table-cell;
-  vertical-align: middle;
-  font-weight: 700;
-  text-transform: uppercase;
-  font-size: 13px;
-  text-align: center;
-  color: rgba(255, 255, 255, 1);
-  width: ${props => props.width || null};
-  height: 35px;
-  border: none;
-  border-radius: 5px;
-  cursor: ${props => (props.disabled ? "default" : "pointer")};
-  opacity: ${props => (props.disabled ? 0.4 : 1)};
-  background-color: #00a4ea;
-  transition: all 0.3s ease;
-  
-`;
+const FormButton = withStyles((theme) => ({
+    root: {
+        color: '#ffffff',
+        backgroundColor: '#00a4ea',
+        '&:hover': {
+            backgroundColor: '#0d7bea',
+        },
+        margin: 20,
+    },
+}))(Button);
 
-export default Button;
+export default FormButton;
