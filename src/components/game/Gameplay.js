8724e3ea09c11e484912c8843df92a64845bd5a2
Jonas Wittwer <j94wittwer@gmail.com>
2020-04-17 09:46:08 +0200

    minor changes in Role Popup

--- src/components/game/Gameplay.js
+++ src/components/game/Gameplay.js
@@ -1,15 +1,14 @@
-import React, {Component} from "react";
-import styled from "styled-components";
-import TimerInfo from "./TimerInfo";
-import PointsInfo from "./PointsInfo";
-import Table from "./Table";
-import {api} from "../../helpers/api";
-import {store} from "../../store";
-import {BaseContainer, GameContainer} from "../../helpers/layout";
-import Button from "../../views/design/Button";
-import RolePopup from "./RolePopup";
-import AllPlayerBoxes from "./AllPlayerBoxes";
-
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import TimerInfo from './TimerInfo';
+import PointsInfo from './PointsInfo';
+import Table from './Table';
+import { api } from '../../helpers/api';
+import { store } from '../../store';
+import { BaseContainer, GameContainer } from '../../helpers/layout';
+import Button from '../../views/design/Button';
+import RolePopup from './RolePopup';
+import AllPlayerBoxes from './AllPlayerBoxes';
 
 const InfoContainer = styled.div`
   display: flex;
@@ -105,7 +104,7 @@ export default class Gameplay extends Component {
                                 this.toggleRolePopup()
                             }}>Toggle Role</Button>
                             {this.state.showRolePopup ?
-                                <RolePopup role={this.props.player.role}
+                                <RolePopup role={this.state.loggedInPlayer.role}
                                            closePopup={this.toggleRolePopup.bind(this)}/> : null}
                             <TimerInfo/>
                         </InfoContainer>
