8a9304173b5a421d341145978507debc23883e81
taylor <taylor.mccants@uzh.ch>
2020-04-24 18:31:23 +0200

    FS20G03-21 #comment leaderboard added but not sorted on score or ranked

---
+++ src/components/leaderboard/LeaderboardTable.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import LeaderboardRow from "./LeaderboardRow";
+
+class LeaderboardTable extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <TableContainer style={{ maxHeight: 400 }}>
+        <Table
+          style={{ minWidth: 650 }}
+          size="small"
+          aria-label="dense table"
+          stickyHeader
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">User Id</TableCell>
+              <TableCell align="center">Username</TableCell>
+              <TableCell align="center">Join Date</TableCell>
+              <TableCell align="center">Score</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.props.users.map((user) => (
+              <LeaderboardRow
+                key={user.id}
+                user={user}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
+}
+
+export default LeaderboardTable;
