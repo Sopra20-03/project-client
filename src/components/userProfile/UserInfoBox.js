0ceaf394bc36b7457d0de7d3f0deb52c08f081a0
taylor <taylor.mccants@uzh.ch>
2020-05-06 19:03:18 +0200

    FS20G03-24 #comment linked avatar image with profile icon field

--- src/components/userProfile/UserInfoBox.js
+++ src/components/userProfile/UserInfoBox.js
@@ -1,11 +1,26 @@
 import React from "react";
 import styled from "styled-components";
-import {BaseContainer} from "../../helpers/layout";
 import {PlayerContainer} from "../game/PlayerBox";
 import Colors from "../../views/design/Colors/Colors";
 import butterfly from '../../views/logos/010-butterfly.png';
 import Button from "../../views/design/Button";
 import {ContainerRow} from "../game/Gameplay";
+import {api, handleError} from "../../helpers/api";
+import { connect } from "react-redux";
+import {getUserDetails} from "../../redux/actions/userActions";
+import userfemale from "../../views/logos/user_female.png";
+import usermale from "../../views/logos/user_male.png";
+import bird from "../../views/logos/001-bird.png";
+import dog from "../../views/logos/002-dog.png";
+import cat from "../../views/logos/003-cat.png";
+import fish from "../../views/logos/004-clown-fish.png";
+import iguana from "../../views/logos/005-iguana.png";
+import hen from "../../views/logos/006-hen.png";
+import owl from "../../views/logos/007-owl.png";
+import bee from "../../views/logos/008-bee.png";
+import swan from "../../views/logos/009-swan.png";
+import robot from "../../views/logos/robot.png";
+
 
 const UserInfoContainer = styled.div`
   background-color: #f3ead8;
@@ -13,9 +28,7 @@ const UserInfoContainer = styled.div`
   border-radius: 5px 5px 5px 5px;
   padding: 10px;
   position: relative;
-  width: 35%;
-  margin-bottom: 2rem;
-  margin-top: 1rem;
+  width: 50%;
   border: 2px solid ${Colors.black};
 `;
 
@@ -23,14 +36,111 @@ const UserInfoGrid = styled.div`
   display: grid;
   grid-template-columns: 50% 50% 1fr;
   grid-gap: 5px;
+  margin-right: 10px;
 `;
 
-const InfoTable = styled.div`
+const InfoTable = styled.table`
     text-align: left;
+    width: 80%;
+`;
+
+const ProfileTextInput = styled.input`
+  background-color: #f3ead8;
+  border: none;
+  color: #0d0d0d;
+  padding: 0.1rem 0.1rem;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 14px;
+  width: 100%;
+  border: 2px solid;
+  transition: all 0.5s ease-in-out;
+  border-radius: 5px;
+
+  &::focus {
+    background-color: #fff;
+    border-bottom: 2px solid #5fbae9;
+    border-radius: 5px;
+  }
 `;
 
 
 class UserInfoBox extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            editMode: false,
+            name: null,
+            username: null,
+            icon: null
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            name: this.props.user.name,
+            username: this.props.user.username,
+            icon: this.props.user.icon
+        })
+    }
+
+    async updateProfile() {
+        try {
+            const requestBody = {
+                name: this.state.name,
+                username: this.state.username,
+                icon: this.state.icon
+            };
+            const response = await api.put(`/users/${this.props.user.id}`, requestBody, {
+                withCredentials: true,
+            });
+            this.setState({editMode: false});
+            await this.props.getUserDetails(this.props.user.id);
+        } catch (error) {
+            alert(handleError(error));
+        }
+    }
+
+    changeToEditMode() {
+        this.setState({editMode: true});
+    }
+
+    handleInputChange(key, value) {
+        this.setState({[key]: value});
+    }
+
+    chooseIcon() {
+        switch(this.state.icon){
+            case "female":
+                return userfemale;
+            case "male":
+                return usermale;
+            case "bird":
+                return bird;
+            case "dog":
+                return dog;
+            case "cat":
+                return cat;
+            case "fish":
+                return fish;
+            case "iguana":
+                return iguana;
+            case "hen":
+                return hen;
+            case "owl":
+                return owl;
+            case "bee":
+                return bee;
+            case "swan":
+                return swan;
+            case "butterfly":
+                return butterfly;
+            default:
+                return robot;
+        }
+    }
 
     render() {
         return (
@@ -38,12 +148,12 @@ class UserInfoBox extends React.Component {
                 <UserInfoContainer>
                     <h3>WELCOME {this.props.user.name} !</h3>
                     <UserInfoGrid>
-                        <PlayerContainer style={{border: `2px solid ${Colors.blue}`, justifySelf: 'center'}}>
-                            {<img alt="" src={butterfly} height="60rem" width="50rem"/>}
+                        <PlayerContainer style={{border: `2px solid ${Colors.blue}`, justifySelf: 'center', margin: 0}}>
+                            {<img alt="" src={this.chooseIcon()} height="60rem" width="50rem"/>}
                             <h4 style={{margin: "0"}}>{this.props.user.username}</h4>
                         </PlayerContainer>
                         <div>
-                            <table style={{textAlign: 'left'}}>
+                            <InfoTable>
                                 <tr>
                                     <td style={{width: '55%'}}>
                                         <span style={Colors.textOrange}>User Id : </span>
@@ -54,33 +164,67 @@ class UserInfoBox extends React.Component {
                                 </tr>
                                 <tr>
                                     <td>
-                                        <span style={Colors.textRed}>Name : </span>
+                                        <span style={Colors.textRed}>Username : </span>
                                     </td>
                                     <td>
-                                        {this.props.user.name}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <span style={Colors.textPink}>Icon : </span>
-                                    </td>
-                                    <td>
-                                        Butterfly
+                                        {this.state.editMode ? (
+                                            <ProfileTextInput onChange={e => {
+                                                this.handleInputChange('username', e.target.value);
+                                            }} type='text' placeholder='Username' defaultValue={this.props.user.username}></ProfileTextInput>
+                                        ) : (
+                                            this.props.user.username
+                                        )}
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
-                                        <span style={Colors.textViolet}>Status : </span>
+                                        <span style={Colors.textPink}>Name : </span>
+                                    </td>
+                                    <td>
+                                        {this.state.editMode ? (
+                                            <ProfileTextInput onChange={e => {
+                                                this.handleInputChange('name', e.target.value);
+                                            }} type='text' placeholder='Name' defaultValue={this.props.user.name}></ProfileTextInput>
+                                        ) : (
+                                            this.props.user.name
+                                        )}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <span style={Colors.textViolet}>Icon : </span>
+                                    </td>
+                                    <td>
+                                        {this.state.editMode ? (
+                                            <ProfileTextInput onChange={e => {
+                                                this.handleInputChange('icon', e.target.value);
+                                            }} type='text' placeholder='Icon' defaultValue={this.state.icon}></ProfileTextInput>
+                                        ) : (
+                                            this.state.icon
+                                        )}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <span style={Colors.textBlue}>Status : </span>
                                     </td>
                                     <td>
                                         <span
                                             style={this.props.user.status === 'ONLINE' ? Colors.textGreen : Colors.textBlack}>{this.props.user.status}</span>
                                     </td>
                                 </tr>
-                            </table>
+                            </InfoTable>
                         </div>
                     </UserInfoGrid>
-                    <Button>Edit</Button>
+                    {this.state.editMode ? (
+                        <Button style={{margin: 10}} onClick={() => {
+                            this.updateProfile();
+                        }}>Update</Button>
+                    ) : (
+                        <Button style={{margin: 10}} onClick={() => {
+                            this.changeToEditMode();
+                        }}>Edit</Button>
+                    )}
                 </UserInfoContainer>
             </ContainerRow>
         );
@@ -88,4 +232,4 @@ class UserInfoBox extends React.Component {
 
 }
 
-export default (UserInfoBox);
+export default connect(null, { getUserDetails })(UserInfoBox);
