bbe69626f0c0ab3d6cde749cb787d3a975f11d79
taylor <taylor.mccants@uzh.ch>
2020-05-18 23:51:59 +0200

    FS20G03-74 #comment added minimized view and send message on enter

--- src/components/chat/chatbox.js
+++ src/components/chat/chatbox.js
@@ -9,11 +9,10 @@ import {
     MessageText,
     TextComposer,
     Row,
-    SendButton,
-    EmojiIcon,
     IconButton, SendIcon
 } from "@livechat/ui-kit";
-import {TextInput} from "../login/Login";
+import MinimizeIcon from '@material-ui/icons/Minimize';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
 import {api, handleError} from "../../helpers/api";
 import {connect} from "react-redux";
 import userIcons, {chooseIcon} from '../../views/design/UserIcons';
@@ -29,9 +28,35 @@ export const ChatContainer = styled.div`
   border-radius: 5px 5px 5px 5px;
   border: 2px solid ${Colors.orange};
   padding: 1.2rem;
-  padding-bottom: 5rem;
+  padding-bottom: 6rem;
   width: 500px;
   height: 400px;
+`;
+
+export const MiniChatContainer = styled.div`
+  margin: 1em;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(255,123,0,0.5);
+  --webkit-border-radius: 10px 10px 10px 10px;
+  border-radius: 5px 5px 5px 5px;
+  border: 2px solid ${Colors.orange};
+  width: 100px;
+`;
+
+export const TextField = styled.input`
+  background-color: #f3ead8;
+  border: none;
+  color: #0d0d0d;
+  padding: 0.2rem 0.6rem;
+  margin: 0.5rem 1rem;
+  text-align: center;
+  display: inline-block;
+  font-size: 16px;
+  width: 85%;
+  border: 2px solid;
+  border-radius: 5px;
 `;
 
 class ChatBox extends Component {
@@ -40,6 +65,7 @@ class ChatBox extends Component {
         this.state = {
             message: '',
             messageList: null,
+            minimized: false,
             theme: {
                 vars: {
                     'primary-color': '#427fe1',
@@ -86,15 +112,14 @@ class ChatBox extends Component {
         };
         try {
             const response = await api.post(
-                `/games/${this.props.gameState.gameId}/messages`,
+                `/games/${this.props.lobbyState.gameId}/messages`,
                 requestBody,
                 {
                     withCredentials: true,
                 }
             );
             console.log("Message sent: ", response.data);
-            //TODO: CLEAR MESSAGE BOX AFTER MESSAGE SENT
-
+            this.setState({message: ''});
         } catch (error) {
             console.log(handleError(error));
         }
@@ -102,52 +127,101 @@ class ChatBox extends Component {
 
     getTime(dateInfo) {
         let date = new Date(dateInfo);
-        return date.getHours() + ":" + date.getMinutes();
+        return date.toTimeString().substr(0, 5);
+    }
+
+    isOwner(username) {
+        return username === this.props.userState.user.username;
     }
 
     handleInputChange(value) {
         this.setState({message: value});
     }
 
+    onEnterPress = (event) => {
+        if (event.key === 'Enter') {
+            this.sendMessage();
+        }
+    };
+
+    minimizeMessages = () => {
+        this.setState({minimized: true});
+        console.log('*********************************Minimized chat');
+    };
+
+    maximizeMessages = () => {
+        this.setState({minimized: false});
+    };
+
     render() {
-        return (
-            <div>
-                {this.props.messageList ? (
-                    <ChatContainer>
-                        <ThemeProvider vars={this.state.theme.vars}>
-                                <MessageList active style={{backgroundColor: 'transparent'}}>
+        if (this.state.minimized) {
+            return (
+                <MiniChatContainer>
+                    <ThemeProvider vars={this.state.theme.vars}>
+                            <IconButton onClick={this.maximizeMessages}>
+                                <ChatBubbleIcon style={{color: Colors.blue}}/>
+                            </IconButton>
+                    </ThemeProvider>
+                </MiniChatContainer>
+            )
+        } else {
+            return (
+                <div>
+                    {this.props.messageList ? (
+                        <ChatContainer>
+                            <ThemeProvider vars={this.state.theme.vars}>
+                                <Row reverse>
+                                    <IconButton onClick={this.minimizeMessages}>
+                                        <MinimizeIcon style={{color: Colors.blue}}/>
+                                    </IconButton>
+                                </Row>
+                                <MessageList active style={{backgroundColor: 'transparent', color: 'black'}}>
                                     {this.props.messageList.map((message) => (
-                                        <MessageGroup avatar={chooseIcon(message.icon ? message.icon : "dog")}
-                                                      isOwn={message.username === this.props.userState.user.username}
-                                                      style={{marginBottom: 0}}>
-                                            <Message date={this.getTime(message.timeCreated)}
-                                                     isOwn={message.username === this.props.userState.user.username}
-                                                     authorName={message.username}>
-                                                <MessageText style={{padding: 0, backgroundColor: 'white'}}>{message.text}</MessageText>
-                                            </Message>
-                                        </MessageGroup>
+                                        <Row reverse={this.isOwner(message.username)}>
+                                            <MessageGroup avatar={chooseIcon(message.icon ? message.icon : "dog")}
+                                                          isOwn={this.isOwner(message.username)}
+                                                          style={{marginBottom: 0}}>
+                                                <Message date={this.getTime(message.timeCreated)}
+                                                         isOwn={this.isOwner(message.username)}
+                                                         authorName={message.username}
+                                                         style={{
+                                                             backgroundColor: 'white',
+                                                             borderRadius: '30px',
+                                                             paddingLeft: '10px',
+                                                             paddingRight: '10px',
+                                                             paddingTop: '5px',
+                                                             paddingBottom: '5px'
+                                                         }}>
+                                                    <MessageText style={{padding: 0}}>{message.text}</MessageText>
+                                                </Message>
+                                            </MessageGroup>
+                                        </Row>
                                     ))}
                                 </MessageList>
-                                <TextComposer style={{backgroundColor: 'transparent'}}>
-                                    <Row align="center">
-                                        <TextInput fill
+                                <TextComposer style={{backgroundColor: 'transparent', padding: '0px'}}>
+                                    <Row align="center" style={{padding: '5px'}}>
+                                        <TextField fill
+                                                   value={this.state.message}
+                                                   style={{textAlign: 'left'}}
+                                                   onKeyPress={this.onEnterPress}
                                                    onChange={(e) => {
                                                        this.handleInputChange(e.target.value);
                                                    }}/>
                                         <IconButton fit onClick={this.sendMessage}>
-                                            <SendIcon color={Colors.blue} />
+                                            <SendIcon color={Colors.blue}/>
                                         </IconButton>
 
                                     </Row>
                                 </TextComposer>
-                        </ThemeProvider>
-                    </ChatContainer>
-                ) : (<div/>)
-                }
-            </div>
-        )
-            ;
+                            </ThemeProvider>
+                        </ChatContainer>
+                    ) : (<div/>)
+                    }
+                </div>
+            )
+        }
     }
+
 
 }
 
